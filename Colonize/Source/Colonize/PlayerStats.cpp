9e12b26b28b1baa7d7911e49b7ece3297bb4fc82
Jad Khalili <jad@videolab.ae>
2018-10-06 16:40:04 +0400

    Add player stats definitions

---
+++ Colonize/Source/Colonize/PlayerStats.cpp
@@ -0,0 +1,138 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PlayerStats.h"
+
+
+// Sets default values for this component's properties
+UPlayerStats::UPlayerStats()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPlayerStats::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UPlayerStats::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+float UPlayerStats::GetLevel()
+{
+	return Level;
+}
+
+float UPlayerStats::SetLevel(float NewLevel)
+{
+	Level = NewLevel;
+	return Level;
+}
+
+float UPlayerStats::GetTotalPopulation()
+{
+	return TotalPopulation;
+}
+
+float UPlayerStats::SetTotalPopulation(float NewTotalPopulation)
+{
+	TotalPopulation = NewTotalPopulation;
+	return TotalPopulation;
+}
+
+float UPlayerStats::AddTotalPopulation(float Population)
+{
+	TotalPopulation += Population;
+	return TotalPopulation;
+}
+
+float UPlayerStats::SubtractTotalPopulation(float Population)
+{
+	TotalPopulation -= Population;
+	return TotalPopulation;
+}
+
+float UPlayerStats::GetTotalCredits()
+{
+	return TotalCredits;
+}
+
+float UPlayerStats::SetTotalCredits(float NewTotalCredits)
+{
+	TotalCredits = NewTotalCredits;
+	return TotalCredits;
+}
+
+float UPlayerStats::AddTotalCredits(float Credits)
+{
+	TotalCredits += Credits;
+	return Credits;
+}
+
+float UPlayerStats::SubtractTotalCredits(float Credits)
+{
+	TotalCredits -= Credits;
+	return TotalCredits;
+}
+
+float UPlayerStats::GetTotalMarble()
+{
+	return TotalMarble;
+}
+
+float UPlayerStats::SetTotalMarble(float NewMarble)
+{
+	TotalMarble = NewMarble;
+	return TotalMarble;
+}
+
+float UPlayerStats::AddTotalMarble(float Marble)
+{
+	TotalMarble += Marble;
+	return TotalMarble;
+}
+
+float UPlayerStats::SubtractTotalMarble(float Marble)
+{
+	TotalMarble -= Marble;
+	return TotalMarble;
+}
+
+TArray<AActor*> UPlayerStats::GetColonyActors()
+{
+	return ColonyActors;
+}
+
+TArray<AActor*> UPlayerStats::AddColonyToTrack(AActor * Colony)
+{
+	ColonyActors.Add(Colony);
+	return ColonyActors;
+}
+
+TArray<AActor*> UPlayerStats::DeleteColonyToTrack(AActor * Colony)
+{
+	for (uint32 i = 0; i < ColonyActors.GetAllocatedSize(); i++)
+	{
+		if (ColonyActors[i] == Colony)
+		{
+			ColonyActors.Remove(Colony);
+		}
+	}
+
+	return ColonyActors;
+}
+
---
+++ Colonize/Source/Colonize/PlayerStats.h
@@ -0,0 +1,90 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "Runtime/Core/Public/Containers/Array.h"
+#include "Runtime/Core/Public/GenericPlatform/GenericPlatform.h"
+#include "PlayerStats.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class COLONIZE_API UPlayerStats : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UPlayerStats();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable)
+	float GetLevel();
+
+	UFUNCTION(BlueprintCallable)
+	float SetLevel(float NewLevel);
+
+
+	UFUNCTION(BlueprintCallable)
+	float GetTotalPopulation();
+
+	UFUNCTION(BlueprintCallable)
+	float SetTotalPopulation(float NewTotalPopulation);
+
+	UFUNCTION(BlueprintCallable)
+	float AddTotalPopulation(float Population);
+
+	UFUNCTION(BlueprintCallable)
+	float SubtractTotalPopulation(float Population);
+
+
+	UFUNCTION(BlueprintCallable)
+	float GetTotalCredits();
+
+	UFUNCTION(BlueprintCallable)
+	float SetTotalCredits(float NewTotalCredits);
+
+	UFUNCTION(BlueprintCallable)
+	float AddTotalCredits(float Credits);
+
+	UFUNCTION(BlueprintCallable)
+	float SubtractTotalCredits(float Credits);
+
+
+	UFUNCTION(BlueprintCallable)
+	float GetTotalMarble();
+
+	UFUNCTION(BlueprintCallable)
+	float SetTotalMarble(float NewMarble);
+
+	UFUNCTION(BlueprintCallable)
+	float AddTotalMarble(float Marble);
+
+	UFUNCTION(BlueprintCallable)
+	float SubtractTotalMarble(float Marble);
+
+
+	UFUNCTION(BlueprintCallable)
+	TArray<AActor*> GetColonyActors();
+
+	UFUNCTION(BlueprintCallable)
+	TArray<AActor*> AddColonyToTrack(AActor* Colony);
+
+	UFUNCTION(BlueprintCallable)
+	TArray<AActor*> DeleteColonyToTrack(AActor* Colony);
+
+private:
+	float Level;
+	float TotalPopulation;
+	float TotalCredits;
+	float TotalMarble;
+	TArray<AActor*> ColonyActors;	
+};
